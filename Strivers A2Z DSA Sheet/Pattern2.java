b7c37b9d75fa1edb9cbef918750f4152580cea57
amitkumar18596 <maharana.amit96@gmail.com>
2024-11-13 12:10:07 +0530

    Pattern 2

---
+++ Strivers A2Z DSA Sheet/Pattern2.java
@@ -0,0 +1,13 @@
+//https://www.naukri.com/code360/problems/n-2-forest_6570178?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+
+public class Pattern2 {
+    public static void nForest(int n) {
+        // Write your code here
+        for (int i=0;i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
