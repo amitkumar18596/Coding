e931d35a6212aa9eb868a943db23745f5a434079
amitkumar18596 <maharana.amit96@gmail.com>
2024-11-14 12:16:43 +0530

    Pattern 9

---
+++ Strivers A2Z DSA Sheet/Pattern9.java
@@ -0,0 +1,24 @@
+//https://www.naukri.com/code360/problems/star-diamond_6573686?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+
+public class Pattern9 {
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+        for (int k=0;k<2*i+1;k++)
+            System.out.print("*");
+        for (int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+
+        System.out.println("\n");
+    }
+     for (int i=0;i<n;i++){
+        for (int j=0;j<i;j++)
+            System.out.print(" ");
+        for (int k=0;k<((n*2)-(2*i+1));k++)
+            System.out.print("*");
+        for (int j=0;j<i;j++)
+            System.out.print(" ");
+
+        System.out.println("\n");
+    }
+}
