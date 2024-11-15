7fff7cbf5daef60402cf87a7e2b553d933ab9998
amitkumar18596 <maharana.amit96@gmail.com>
2024-11-16 01:00:37 +0530

    Pattern 16

---
+++ Strivers A2Z DSA Sheet/Pattern16.java
@@ -0,0 +1,13 @@
+public class Pattern16 {
+    public static void alphaRamp(int n) {
+        // Write your code here
+        for (int i=0;i<n;i++){
+            char ch = (char) ('A' + i);
+            for (int j=0;j<=i;j++){
+                System.out.print(ch +" ");
+                
+            }
+            System.out.println("\n");
+        }
+    }
+}
