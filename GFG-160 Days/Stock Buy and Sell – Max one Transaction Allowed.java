16bcd5168799ed659cb6dba3b56a60eeebfc0c17
amitkumar18596 <maharana.amit96@gmail.com>
2024-11-22 03:00:23 +0530

    Stock Buy and Sell – Max one Transaction Allowed

---
+++ GFG-160 Days/Stock Buy and Sell – Max one Transaction Allowed.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int prices[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                prices[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int res = obj.maximumProfit(prices);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // Code here
+        
+        int n = prices.length;
+        
+        int minElement = prices[0];
+        int maxProfit = 0;
+        
+        for (int i=1;i<n;i++){
+            //Find the min the element
+            minElement = Math.min(minElement, prices[i]);
+            
+            //find the max difference
+            maxProfit = Math.max(maxProfit, prices[i]-minElement);
+            
+            
+        }
+        return maxProfit;
+    }
+}
